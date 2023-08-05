63a07d89e361793b4309481cf378306a2eb5adc8
trndav <farsiteguide@gmail.com>
2023-08-05 20:59:15 +0200

    "Added sign up/register form"

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   # since this are all resource routes: resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   # we can use:
   resources :articles
+  get "signup", to: "users#new"
+  # post "users", to: "users#create"
+  # to get all routes, we can call resources :users, without  post: "users" as we know users will have all RESTful routes
+  resources :users, except: [:new]
 end
