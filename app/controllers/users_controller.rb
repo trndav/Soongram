91252977aadf1b08218aacca1701158e8a276c60
trndav <farsiteguide@gmail.com>
2023-08-06 11:29:23 +0200

    Added sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Welcome to the Soongram #{@user.username}, you are registered now."
+            redirect_to articles_path
+        else render "new", status: :unprocessable_entity
+        end
+    end
+    private
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
