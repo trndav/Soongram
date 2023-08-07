9a278ad756536b5d6c2f2d105698389de4d30f67
trndav <farsiteguide@gmail.com>
2023-08-07 11:18:47 +0200

    Added Users listing page, links to users from articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
         @articles = @user.articles
     end
 
+    def index
+        @users = User.all
+    end
+
     def new
         @user = User.new
     end
@@ -15,7 +19,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "You have updated your account #{@user.username}."
-            redirect_to articles_path
+            redirect_to user_path(@user)
         else
             render "edit"
         end
