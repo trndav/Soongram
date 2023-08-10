8e3433b068a819f28627cdbaf0154de253e13910
trndav <farsiteguide@gmail.com>
2023-08-10 10:39:57 +0200

    "Fixed flash messages, green for notice, and red for alert."

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,11 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
-            flash[:alert] = "Logged in successfully."
+            flash[:notice] = "Logged in successfully."
             redirect_to user
         else
             flash.now[:alert] = "There was something wrong with your login details."
-            render "new"
+            render "new", status: :unprocessable_entity
         end
     end
 
